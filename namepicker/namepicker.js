a624e6bfe4f599770876f8f9250ce4f64d0f5901
lckof-pc <suyu0925@gmail.com>
2014-11-26 12:55:31 +0800

    namepicker: remove 3 and 4 words.

--- namepicker/namepicker.js
+++ namepicker/namepicker.js
@@ -14,28 +14,3 @@ for (var i = 0; i < words.length; i++)
 document.writeln("两字组合：");
 document.writeln(names);
 names = "";
-
-// 3 words
-for (var i = 0; i < words.length; i++)
-	for (var j = 0; j < words.length; j++) 
-		for (var k = 0; k < words.length; k++)
-			if (i != j && j != k && i != k) {
-				names += words[i] + words[j] + words[k] + "\r\n";
-			}
-
-document.writeln("三字组合：");
-document.writeln(names);
-names = "";
-
-// 4 words
-for (var i = 0; i < words.length; i++)
-	for (var j = 0; j < words.length; j++) 
-		for (var k = 0; k < words.length; k++) 
-			for (var l = 0; l < words.length; l++)
-				if (i != j && i != k && i != l && j != k && j != l && k != l) {
-					names += words[i] + words[j] + words[k] + words[l] + "\r\n";
-				}
-
-document.writeln("四字组合：");
-document.writeln(names);
-names = "";
